6b1db7ac0f6f8261b447064f2625fa85ee4575fe
Jere Rytkönen <76254067+jerytkon@users.noreply.github.com>
2022-04-13 21:42:06 +0200

    part7 final

--- part7/bloglist/backend/controllers/blogs.js
+++ part7/bloglist/backend/controllers/blogs.js
@@ -16,7 +16,7 @@ const getTokenFrom = request => {
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
       .find({})
-      .populate('user', { username: 1, name: 1 })
+      .populate('user', { username: 1, name: 1, id: 1 })
 
       response.json(blogs)
   })
